c5ada4f638c370a5f50ceb3ffe8200c0f6ae8177
SuetamId <mmcc.silveira@gmail.com>
2024-08-16 15:59:10 -0300

    feat: added swagger docs, task service and task controller with save and list methods

---
+++ src/main/java/com/br/taskmanage/taskmanage_api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.br.taskmanage.taskmanage_api.controller;
+
+import com.br.taskmanage.taskmanage_api.model.User;
+import com.br.taskmanage.taskmanage_api.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @GetMapping
+  public ResponseEntity<List<User>> findAll(){
+      return ResponseEntity.ok(userService.findAll());
+  }
+
+  @PostMapping
+  public ResponseEntity<User> save(@RequestBody User user) {
+      return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
+  }
+}
