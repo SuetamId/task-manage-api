379cfd87d2594c4dcc593012d7ace4febf5f2d01
SuetamId <mmcc.silveira@gmail.com>
2024-08-17 09:55:14 -0300

    chore: added route authentication config

--- src/main/java/com/br/taskmanage/taskmanage_api/infra/security/SecurityConfiguration.java
+++ src/main/java/com/br/taskmanage/taskmanage_api/infra/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.br.taskmanage.taskmanage_api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,10 @@ public class SecurityConfiguration {
         return http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers(
+                        HttpMethod.GET, "/users"
+                ).hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .build();
     }
 }
