0fd492dafd723b43cdaae949dca0b69e06cb175e
SuetamId <mmcc.silveira@gmail.com>
2024-08-19 10:59:42 -0300

    chore: added swagger permission route, in the security configuration

--- src/main/java/com/br/taskmanage/taskmanage_api/infra/security/SecurityConfiguration.java
+++ src/main/java/com/br/taskmanage/taskmanage_api/infra/security/SecurityConfiguration.java
@@ -1,11 +1,11 @@
 package com.br.taskmanage.taskmanage_api.infra.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,11 +16,11 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers(
-                        HttpMethod.GET, "/users"
-                ).hasRole("ADMIN")
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .build();
     }
